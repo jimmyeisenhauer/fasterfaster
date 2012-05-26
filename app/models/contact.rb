cc6f0f41acc5a92e903776c1f3eb3ffe09cb19f7
James Eisenhauer <james.eisenhauer@gmail.com>
2012-05-26 16:30:56 -0700

    cache add

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,16 @@
 class Contact < ActiveRecord::Base
   attr_accessible :comment, :name
+
+  def self.all_cached
+  Rails.cache.fetch('Contact.all') { all }
+  end
+
+  after_save    :expire_contact_all_cache
+  after_destroy :expire_contact_all_cache
+
+  def expire_contact_all_cache
+  Rails.cache.delete('Contact.all')
+  end
+
 end
+
