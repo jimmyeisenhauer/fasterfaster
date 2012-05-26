8752f449ef75264ce029cec0acfc6a1d91369202
James Eisenhauer <james.eisenhauer@gmail.com>
2012-05-26 09:43:54 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fasterfaster::Application.config.secret_token = 'b7fa5afdd172c0fe0b424241495c4bb1adfc6a8b7254d3bba8ecd67edd79971cfdbd038f99ea7c5b6dd77ed07d684dbcdc03938c60008bb4391e257f2d6cd683'
